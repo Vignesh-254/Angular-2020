b33c1c4ce064da8763e149170d61bf29950e07af
Vignesh-254 <vigneshperumal99@gmail.com>
2020-08-19 18:40:20 +0530

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   
   title = 'Training';
  
   data
-  ngOnInit(): void {
-  }
 
   studentDetails(value){
     this.data = value
